59e0942a7782eaf75489c1d4e3a4c1826923efba
Ulf <abdullahakram106@gmail.com>
2024-05-25 20:02:12 +0500

    Added config and minor changes

--- ProfileOpener/ProfileOpener.cpp
+++ ProfileOpener/ProfileOpener.cpp
@@ -5,57 +5,118 @@
 #include <regex>
 #include <windows.h>
 
+using namespace std;
+
 // Function to extract the id64 from a line of text
-std::string extractId64(const std::string& line) {
-    std::regex id64Regex("\\d{17}");
-    std::smatch match;
-    if (std::regex_search(line, match, id64Regex)) {
+string extractId64(const string& line) {
+    regex id64Regex("\\d{17}");
+    smatch match;
+    if (regex_search(line, match, id64Regex)) {
         return match[0];
     }
     return "";
 }
 
 // Function to open a Steam profile in the default web browser
-void openProfile(const std::string& id64) {
-    std::string url = "https://steamcommunity.com/profiles/" + id64;
+void openProfile(const string& id64) {
+    string url = "https://steamcommunity.com/profiles/" + id64;
     ShellExecuteA(NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
 }
 
+//M_Config stands for make_config and simply creates a new config file
+void M_Config(int HowMany = 30)
+{
+    ofstream MakeConfig("Config.txt");
+    MakeConfig << HowMany;
+    MakeConfig.close();
+}
+
 int main() {
-    // Open or create the input file
-    std::ifstream inputFile("players.txt");
-    std::ofstream outputFile("players.txt", std::ios_base::app);
-    if (!inputFile.is_open()) {
-        std::cerr << "Failed to open input file. Creating a new one." << std::endl;
-        inputFile.close();
-        std::cout << "Enter the Steam player data. Press Ctrl+Z and Enter to save and exit." << std::endl;
-        std::string line;
-        while (std::getline(std::cin, line)) {
-            if (line.empty()) break; // Break the loop when Ctrl+Z is pressed
-            outputFile << line << std::endl;
-        }
-        outputFile.close();
+
+    cout << "Initializing the program...\n\n";
+
+    // Open the input file
+    ifstream InputFile("players.txt");
+
+    if (!InputFile.is_open()) {
+
+        cerr << "Failed to open input file, a new file will be created\n\n";
+
+        InputFile.close();
+        ofstream OutputFile("players.txt");
+
+        cout << "The file has been created with the name 'player.txt', Enter the data in the file and launch the program again\n\n";
+
+        OutputFile.close();
         return 0;
     }
 
+    int Choice{ 0 };
+
+    a:
+    cout << "Press 1 to change config\n";
+    cout << "Press 2 to continue with the program\n";
+    cout << "Your choice: ";
+    cin >> Choice;
+
+    if (Choice == 1)
+    {
+        int After{ 0 };
+
+        cout << "Enter the number of profiles after which you want to program to pause: ";
+        cin >> After;
+
+        M_Config(After);
+    }
+    if (Choice != 1 && Choice != 2)
+    {
+        cout << "Invalid choice, try again\n";
+        goto a;
+    }
+
+    ifstream ConfigFile("Config.txt");
+
+    if (!ConfigFile.is_open())
+    {
+        cout << "Config file not found, one will be now created\n";
+
+        int After{ 0 };
+
+        cout << "Enter the number of profiles after which you want to program to pause: ";
+        cin >> After;
+
+        M_Config(After);
+
+        ConfigFile.close();
+        ConfigFile.open("Config.txt");
+    }
+
+    cout << "The program is ready to start opening files\n";
+    system("PAUSE");
+
     // Read the data from the file
-    std::vector<std::string> lines;
-    std::string line;
-    while (std::getline(inputFile, line)) {
+    vector<string> lines;
+    string line;
+    while (getline(InputFile, line)) {
         lines.push_back(line);
     }
-    inputFile.close();
+    InputFile.close();
 
     // Extract the id64 from each line and open the profile in the browser
     int Times{ 0 };
+
+    int StopAfter;
+
+    ConfigFile >> StopAfter;
+    
     for (const auto& line : lines) {
-        std::string id64 = extractId64(line);
+        string id64 = extractId64(line);
         if (!id64.empty()) {
             openProfile(id64);
         }
-        if (Times == 30)
+        if (Times == StopAfter)
         {
-            std::cout << "30 profiles have been opened, press any key when you are ready to open the next 30\n";
+            cout << StopAfter << " profiles have been opened, press any key when you are ready to open the next " << StopAfter << "\n";
             Times = 0;
             system("PAUSE");
         }
